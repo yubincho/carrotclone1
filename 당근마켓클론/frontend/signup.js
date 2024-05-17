b8c26eab1be4b69700a66df6e01100e0e3ae1fbf
yubincho <yubincho9@gmail.com>
2024-05-17 22:05:40 +0900

    당근마켓클론 - 회원가입 로직 구현

--- 당근마켓클론/frontend/signup.js
+++ 당근마켓클론/frontend/signup.js
@@ -27,12 +27,14 @@ const handleSubmit = async (event) => {
             method: "POST",
             body: formData,
         })
-        console.log("[res.json()]", res.json())
-        const data = res.json()
-        
+        // console.log("[res.json()]", res.json())
+        const data = await res.json()
+
         if (data === '200') {
             div.innerText = "회원가입에 성공했습니다."  
             div.style.color = "blue"
+            alert("회원가입에 성공하였습니다")
+            window.location.pathname = "/login.html"
         } 
     } else {
         div.innerText = "비밀번호가 같지 않습니다."
--- 당근마켓클론/main.py
+++ 당근마켓클론/main.py
@@ -61,8 +61,17 @@ async def get_image(item_id):
 
 
 @app.post("/signup")
-def signup(id: Annotated[str, Form()], password: Annotated[str, Form()]):
+def signup(id: Annotated[str, Form()], 
+           password: Annotated[str, Form()],
+           name: Annotated[str, Form()],
+           email: Annotated[str, Form()]
+           ):
     print(id, password)
+    cur.execute(f"""
+                INSERT INTO users(id, name, email, password)
+                VALUES('{id}', '{name}', '{email}', '{password}')
+                """)
+    con.commit()
     return "200"
 
 
